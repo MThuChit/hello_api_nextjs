9067abe88a85eef2c530dd61a3db1ffeee4c86da
MThuChit <maythu111016@gmail.com>
2026-02-06 16:09:13 +0700

    update profile

--- src/lib/cors.js
+++ src/lib/cors.js
@@ -1,7 +1,13 @@
-let corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    "Access-Control-Max-Age": "86400",
+const allowedOrigin = process.env.FRONTEND_ORIGIN || "http://localhost:5173";
+ 
+const corsHeaders = {
+  "Access-Control-Allow-Credentials": "true",
+  "Access-Control-Allow-Origin": allowedOrigin,
+  "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Max-Age": "86400",
 };
+ 
 export default corsHeaders;
+ 
+ 
